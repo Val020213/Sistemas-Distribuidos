9cdff2ed2da664c3f1266be39da514db672db56f
Osvaldo R.Moreno P. <osvaldo020213@gmail.com>
2024-11-29 00:19:16 -0500

    bug:integration broken in docker network

--- src/client/src/app/service/scrapper/page.tsx
+++ src/client/src/app/service/scrapper/page.tsx
@@ -1,17 +1,36 @@
+'use client'
 import { ScrapperContainer } from '@/modules/system/service/scrapper/ScrapperContainer'
 import { listUrlService } from '@/services/url-service'
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
+import React, { useEffect, useState } from 'react'
 
-export const metadata: Metadata = {
-  title: 'Scrapper Service',
-  description: 'Scrapper url service',
-}
+// export const metadata: Metadata = {
+//   title: 'Scrapper Service',
+//   description: 'Scrapper url service',
+// }
 
-export default async function Page() {
-  const response = await listUrlService()
+export default function Page() {
+  const [response, setResponse] = useState({})
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const result = await listUrlService()
+        setResponse(result)
+      }
+      fetchData()
+    } catch (error) {
+      console.error(error)
+      setResponse({ statusCode: 500, data: error })
+    }
+  }, [])
+
+  if (response === {}) {
+    return <div>Loading...</div>
+  }
 
   if (response.statusCode !== 200) {
-    return new Error('Error fetching data')
+    return <div>{response.data}</div>
   }
   return <ScrapperContainer data={response.data.body} />
 }
