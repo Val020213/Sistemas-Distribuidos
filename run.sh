990f905ac823756245a5257782515eb5f0679274
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2024-12-03 19:08:02 -0500

    fix/save images system

--- run.sh
+++ run.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 cd ./src || exit 1
 
-docker-compose down -v # en caso de que ya exista un contenedor con el mismo nombre
-docker-compose up --build
+# Verificar si las imágenes existen
+if ! docker images | grep -q "my-router-image"; then
+    echo "Construyendo la imagen my-router-image..."
+    docker-compose build router
+fi
+
+if ! docker images | grep -q "my-backend-image"; then
+    echo "Construyendo la imagen my-backend-image..."
+    docker-compose build backend
+fi
+
+if ! docker images | grep -q "my-frontend1-image"; then
+    echo "Construyendo la imagen my-frontend1-image..."
+    docker-compose build frontend1
+fi
+
+if ! docker images | grep -q "my-frontend2-image"; then
+    echo "Construyendo la imagen my-frontend2-image..."
+    docker-compose build frontend2
+fi
+
+# Levantar los contenedores sin volver a construir las imágenes
+docker-compose up
 
 sleep 10 # Delay para esperar a que los contenedores estén listos
 xdg-open http://localhost:3000 # Abrir el navegador para el frontend1
