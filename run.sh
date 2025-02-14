dda3483ae202d0cc0044fab09db28f225f1e18d4
Osvaldo R.Moreno P. <osvaldo020213@gmail.com>
2025-02-14 04:20:54 -0500

    tmp

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ./src || exit 1
 
+chmod +x ./scrapper_server/server.sh
+
+
 # Verificar si las imágenes existen
 if ! docker images | grep -q "scrapper-router-image"; then
     echo "Construyendo la imagen scrapper-router-image..."
@@ -14,7 +17,7 @@ fi
 
 if ! docker images | grep -q "scrapper-client-image"; then
     echo "Construyendo la imagen scrapper-client-image..."
-    docker-compose build frontend1
+    docker-compose build frontend
 fi
 
 # Levantar los contenedores sin volver a construir las imágenes
