8e33f0ba89f76a0f69414725971ac952b080ebf9
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2025-02-15 10:55:52 -0500

    save again

--- old/save.bat
+++ old/save.bat
@@ -1,27 +1,29 @@
-@echo off
+@REM @echo off
 
-REM 
+@REM REM 
 
-REM Guardar la imagen my-router-image en un archivo tar
-docker save -o scrapper-router-image.tar scrapper-router-image:latest
-if %ERRORLEVEL% NEQ 0 (
-    echo "Error al guardar la imagen scrapper-router-image"
-    exit /b %ERRORLEVEL%
-)
+@REM REM Guardar la imagen my-router-image en un archivo tar
+@REM docker save -o scrapper-router-image.tar scrapper-router-image:latest
+@REM if %ERRORLEVEL% NEQ 0 (
+@REM     echo "Error al guardar la imagen scrapper-router-image"
+@REM     exit /b %ERRORLEVEL%
+@REM )
 
-REM Guardar la imagen my-backend-image en un archivo tar
-docker save -o scrapper-backend-image.tar scrapper-backend-image:latest
-if %ERRORLEVEL% NEQ 0 (
-    echo "Error al guardar la imagen my-backend-image"
-    exit /b %ERRORLEVEL%
-)
+@REM REM Guardar la imagen my-backend-image en un archivo tar
+@REM docker save -o scrapper-backend-image.tar scrapper-backend-image:latest
+@REM if %ERRORLEVEL% NEQ 0 (
+@REM     echo "Error al guardar la imagen my-backend-image"
+@REM     exit /b %ERRORLEVEL%
+@REM )
 
-REM Guardar la imagen my-frontend1-image en un archivo tar
-docker save -o scrapper-client-image.tar scrapper-client-image:latest
-if %ERRORLEVEL% NEQ 0 (
-    echo "Error al guardar la imagen scrapper-client-image"
-    exit /b %ERRORLEVEL%
-)
+@REM REM Guardar la imagen my-frontend1-image en un archivo tar
+@REM docker save -o scrapper-client-image.tar scrapper-client-image:latest
+@REM if %ERRORLEVEL% NEQ 0 (
+@REM     echo "Error al guardar la imagen scrapper-client-image"
+@REM     exit /b %ERRORLEVEL%
+@REM )
 
 
-echo "Todas las imágenes se han guardado y copiado correctamente."
+@REM echo "Todas las imágenes se han guardado y copiado correctamente."
+
+docker sa
